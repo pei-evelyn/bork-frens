8b993c7f44b30010ba4907c8ab158acb6d0eed81
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-10-05 16:32:53 -0700

    Merged inital bg and nav components

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -10,7 +10,7 @@ class Header extends React.Component {
 
   render() {
     return (
-      <div className="header col-12 text-white d-flex flex-wrap justify-content-between container pt-3">
+      <div className="header col-12 text-white d-flex flex-wrap justify-content-between container pt-3 mb-5">
         <i className="fas fa-angle-left fa-2x"></i>
         <h5 className="mt-1">Frens List</h5>
         <i className="fa fa-bars fa-2x" ></i>
