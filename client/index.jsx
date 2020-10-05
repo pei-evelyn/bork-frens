5e25bf4035e4dee4d36596c76770799db795234e
jaimegsandoval <jaime.devs@gmail.com>
2020-10-05 15:35:49 -0700

    Added footer component

--- client/index.jsx
+++ client/index.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Footer from './components/footer';
 import App from './components/app';
 
 ReactDOM.render(
-  <App>
-    <Footer />
-  </App>,
+  <App />,
   document.querySelector('#root')
 );
