fe2661a098197dbe1e8b30bacda15037613ba344
MattChen1021 <mattchen1021gmail.com>
2020-10-06 01:06:18 +0000

    created message component and get request to messages

--- server/index.js
+++ server/index.js
@@ -19,6 +19,18 @@ app.get('/api/health-check', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/messages', (req, res, next) => {
+
+  const sql = `
+  select *
+  from "messages"
+  `;
+  return db.query(sql)
+    .then(result => res.status(200).json(result.rows))
+    .catch(err => next(err));
+
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
