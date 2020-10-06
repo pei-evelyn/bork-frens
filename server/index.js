188d8809226f0357f95ecaafd1ebb91767c236df
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-10-05 20:38:20 -0700

    Completed get endpoint for seeing fren requests via recipientId

--- server/index.js
+++ server/index.js
@@ -13,9 +13,39 @@ app.use(sessionMiddleware);
 
 app.use(express.json());
 
-app.get('/api/health-check', (req, res, next) => {
-  db.query('select \'successfully connected\' as "message"')
-    .then(result => res.json(result.rows[0]))
+// app.get('/api/health-check', (req, res, next) => {
+//   db.query('select \'successfully connected\' as "message"')
+//     .then(result => res.json(result.rows[0]))
+//     .catch(err => next(err));
+// });
+
+// User can see list of connection requests
+
+app.get('/api/fren-requests/:recipientId', (req, res, next) => {
+  const recipientId = parseInt(req.params.recipientId, 10);
+
+  if (recipientId < 0 || isNaN(recipientId)) {
+    throw (new ClientError('Recipient ID must be valid', 400));
+  }
+
+  const sql = `
+    select "u"."dogName" as "requesterName",
+           "u"."imageUrl" as "requesterImage"
+      from "frenRequests" as "fr"
+      join "users" as "u" on "u"."userId" = "fr"."senderId"
+     where "fr"."recipientId" = $1
+  `;
+
+  const params = [recipientId];
+
+  db.query(sql, params)
+    .then(result => {
+      if (result.rows.length === 0) {
+        next(new ClientError(`Id ${recipientId} returned no messages`, 404));
+      } else {
+        res.status(200).json(result.rows);
+      }
+    })
     .catch(err => next(err));
 });
 
