0629924398ad09c38705ea776d49a98ca47af32d
PaulKillian <psk65lava@gmail.com>
2020-10-06 07:00:23 -0700

    Completed see-nearby-users-back-end

--- server/index.js
+++ server/index.js
@@ -23,6 +23,39 @@ app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
 
+app.get('/api/users', (req, res, next) => {
+  const users = `
+    select "userName",
+          "imageUrl",
+          "location",
+          "dogName"
+    from "users"
+  `;
+  db.query(users)
+    .then(userInfo => {
+      const totalUsers = `
+    select count(*) from "users"`;
+      return db.query(totalUsers).then(total => {
+        if (total.rows[0].count === 1) {
+          res.status(404).json({
+            error: 'No Doggos Nearby'
+          });
+        } else {
+          const allData = {
+            userName: userInfo.rows[0].userName,
+            dogName: userInfo.rows[0].dogName,
+            imageUrl: userInfo.rows[0].imageUrl,
+            location: userInfo.rows[0].location,
+            totalUsers: total.rows[0].count
+          };
+          return allData;
+        }
+      });
+    })
+    .then(result => res.json(result))
+    .catch(err => next(err));
+});
+
 app.use((err, req, res, next) => {
   if (err instanceof ClientError) {
     res.status(err.status).json({ error: err.message });
