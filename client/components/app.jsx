94701920f6d7f01b4f340687ecabca734442f5d7
PaulKillian <psk65lava@gmail.com>
2020-10-05 18:02:15 -0700

    initial build of login page

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,31 +2,43 @@ import React from 'react';
 import Header from './header';
 import Background from './background';
 import Footer from './footer';
+import LoginPage from './login-page';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       message: null,
-      isLoading: true
+      isLoading: true,
+      view: {
+        name: name,
+        params: {}
+      }
     };
+    this.getUserNames = this.getUserNames.bind(this);
   }
 
   componentDidMount() {
-    fetch('/api/health-check')
-      .then(res => res.json())
-      .then(data => this.setState({ message: data.message || data.error }))
-      .catch(err => this.setState({ message: err.message }))
-      .finally(() => this.setState({ isLoading: false }));
+    this.getUserNames();
+  }
+
+  getUserNames() {
+    this.setState({ view: { name: 'login', params: {} } });
   }
 
   render() {
-    return (
-      <>
-        <Header />
-        <Background />
-        <Footer />
-      </>
-    );
+    if (this.state.view.name === 'login') {
+      return (
+        <LoginPage />
+      );
+    } else {
+      return (
+        <>
+          <Header />
+          <Background />
+          <Footer />
+        </>
+      );
+    }
   }
 }
---
+++ client/components/login-page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function LoginPage() {
+
+  return (
+    <div className="login-background pt-5 text-white col-12">
+      <i className="d-flex justify-content-center fas fa-bone fa-3x"></i>
+      <h2 className="d-flex justify-content-center mb-5">borkfrens</h2>
+      <select className="form-control form-control-lg col-10 mx-4">
+        <option className="col-9">Select User</option>
+        <option className="col-9">Select User</option>
+      </select>
+    </div>
+  );
+}
+
+export default LoginPage;
