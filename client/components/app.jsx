806af4fa300351dd540c8ba707e6c9e8d8b3150f
PaulKillian <psk65lava@gmail.com>
2020-10-07 11:24:02 -0700

    update

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,27 +1,72 @@
 import React from 'react';
 import Header from './header';
 import FrenRequestList from './fren-request-list';
-// import Footer from './footer';
-// import Messages from './message';
+import Footer from './footer';
+import Messages from './message';
+import LoginPage from './login-page';
+import NearbyFrensList from './nearby-frens-list';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: 'frenRequestList',
-      params: {}
+      view: {
+        name: 'login',
+        params: {}
+      },
+      user: {}
     };
+    this.setView = this.setView.bind(this);
+    this.addUser = this.addUser.bind(this);
+  }
+
+  addUser(userName) {
+    this.setState({
+      user: {
+        user: userName.user,
+        userId: userName.userId
+      }
+    });
+  }
+
+  setView(name, params) {
+    this.setState({
+      view: {
+        name: name,
+        params: {}
+      }
+    });
   }
 
   render() {
     if (this.state.view === 'frenRequestList') {
       return (
         <>
-          <Header text='Fren Requests'/>
-          <FrenRequestList userId='6'/>
+          <Header text='Fren Requests' />
+          <FrenRequestList userId='6' />
         </>
       );
     }
-
+    if (this.state.view.name === 'frensNearby') {
+      return (
+        <>
+          <Header />
+          <NearbyFrensList userId={this.state.user.userId} />
+          <Footer />
+        </>
+      );
+    } else if (this.state.view.name === 'login') {
+      return (
+        <LoginPage addUser={this.addUser} setView={this.setView} />
+      );
+    } else {
+      return (
+        <>
+          <Header />
+          <Messages />
+          <Footer />
+        </>
+      );
+    }
   }
 }
--- server/index.js
+++ server/index.js
@@ -157,6 +157,50 @@ app.get('/api/users/find-frens/list', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/login', (req, res, next) => {
+  db.query('select "userName", "userId" from "users"')
+    .then(result => {
+      res.status(200).json(result.rows);
+    })
+    .catch(err => next(err));
+});
+
+app.get('/api/users/find-frens/list/:location/:userId', (req, res, next) => {
+  const userId = parseInt(req.params.userId, 10);
+  const location = req.params.location;
+  const users = `
+    select  "userId",
+            "userName",
+            "imageUrl",
+            "location",
+            "dogName"
+            from "users"
+      where "location" = $1 and "userId" != $2
+  `;
+  const params = [location, userId];
+  db.query(users, params)
+    .then(userInfo => {
+      const totalUsers = `
+    select count(*) as "numberOfUsers"
+      from "users"
+      where "userId" != ${userId}`;
+      return db.query(totalUsers).then(total => {
+        const userInt = parseInt(total.rows[0].numberOfUsers);
+        if (userInt < 1) {
+          res.status(404).json({
+            error: 'No Doggos Nearby'
+          });
+          return;
+        }
+        const allData = userInfo.rows[0];
+        allData.totalUsers = total.rows[0].numberOfUsers;
+        return allData;
+      });
+    })
+    .then(result => res.json(result))
+    .catch(err => next(err));
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
