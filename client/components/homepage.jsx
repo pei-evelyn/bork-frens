46c9af80a5022b1b7d61e1eecaf41a19400ceb2d
jaimegsandoval <jaime.devs@gmail.com>
2020-10-07 14:39:23 -0700

    Added the styling for homepage excepts for image

--- client/components/homepage.jsx
+++ client/components/homepage.jsx
@@ -4,6 +4,7 @@ class Homepage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      userProfile: [],
       view: {
         name: 'homepage',
         params: {}
@@ -28,24 +29,46 @@ class Homepage extends React.Component {
   }
 
   getUserProfileData() {
+    // `/api/homepage/${this.state.userProfile.userId}`
     fetch('/api/homepage/7')
       .then(response => response.json())
-      .then(userData => userData)
+      .then(userData => {
+        // console.log(userData);
+        this.setState({
+          userProfile: userData
+        });
+      })
       .catch(err => console.error(err));
   }
 
   render() {
-    return (
-      <div className="container">
-        <div className="row d-flex align-items-end">
-          <div className="col content-container mx-3 mt-4">
-            <div className="homepage-profile-img">
-              <img src="" alt="" />
-            </div>
+    const user = this.state.userProfile;
+    // console.log(user);
+    if (user === []) {
+      return null;
+    }
 
+    return (
+      <>
+        <div className="container">
+          <div className="row d-flex align-items-end">
+            <div className="col content-container mx-3 mt-4">
+              <div className="col-3 homepage-profile-img m-auto">
+                <img src={user.imageUrl} alt={`Image of ${user.dogName}`} />
+              </div>
+              <div className="row name-location-box justify-content-center flex-column align-items-center">
+                <h2 className="font-weight-normal">Kaydo</h2>
+                <p>Los Angeles, CA</p>
+              </div>
+              <div className="profile-btn-box d-flex flex-column align-items-center">
+                <button className="homepage-btn green-btn font-weight-light text-white col-10 rounded my-4 py-2">Edit Profile</button>
+                <button className="homepage-btn grey-btn font-weight-light text-white col-10 rounded mb-4 py-2">Find New Frens</button>
+                <button className="homepage-btn green-btn font-weight-light text-white col-10 rounded mb-4 py-2">View My Frens</button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </>
 
     );
   }
