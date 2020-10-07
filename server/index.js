3bb2b92e849b243a4f5be3c2072658da6b9b3224
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-10-06 21:39:18 -0700

    Completed get endpoint for others profile page

--- server/index.js
+++ server/index.js
@@ -95,6 +95,47 @@ app.get('/api/fren-requests/:recipientId', (req, res, next) => {
     })
     .catch(error => next(error));
 });
+
+// User can see other's profile
+
+app.get('/api/others-profile/:userId', (req, res, next) => {
+  const userId = parseInt(req.params.userId, 10);
+
+  if (userId < 0 || isNaN(userId)) {
+    throw (new ClientError(`User Id ${req.params.userId} is not valid`, 400));
+  }
+
+  const sql = `
+    select "u"."userId",
+          "u"."dogName",
+          "u"."location",
+          "u"."userName",
+          "u"."breed",
+          "u"."DOB",
+          "u"."tagline",
+          "u"."imageUrl",
+          "fl"."level",
+          "g"."identity"
+    from "users" as "u"
+    join "frenlinessLevels" as "fl" using ("levelId")
+    join "genders" as "g" using ("genderId")
+    where "userId" = $1
+  `;
+
+  const params = [userId];
+
+  db.query(sql, params)
+    .then(result => {
+      if (result.rows.length === 0) {
+        next(new ClientError(`User Id ${userId} does not exist`, 404));
+      } else {
+        return res.status(200).json(result.rows[0]);
+      }
+    })
+    .catch(err => next(err));
+
+});
+
 // User can log in to account
 
 app.get('/api/login', (req, res, next) => {
