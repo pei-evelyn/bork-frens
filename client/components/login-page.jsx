9e4fb9d7b2003129bca5fdd25d246182649b3fcd
PaulKillian <psk65lava@gmail.com>
2020-10-08 14:59:49 -0700

    update

--- client/components/login-page.jsx
+++ client/components/login-page.jsx
@@ -33,7 +33,7 @@ class LoginPage extends React.Component {
       user: event.target.value,
       userId: optionId
     });
-    this.props.setView('homepage', {});
+    this.props.setView('frensNearby', {});
   }
 
   render() {
