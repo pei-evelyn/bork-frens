c4c2e4f7ff37fb7c58a139b4f8dbf4db8c9afa09
MattChen1021 <mattchen1021gmail.com>
2020-10-08 19:46:57 +0000

    making some minor edits to css

--- client/components/conversation-list.jsx
+++ client/components/conversation-list.jsx
@@ -4,7 +4,7 @@ export default class ConversationList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userId: '',
+      userId: 6,
       conversations: []
     };
     this.getConversations = this.getConversations.bind(this);
@@ -41,5 +41,12 @@ export default class ConversationList extends React.Component {
       </div>
     );
   }
-
 }
+
+//  const d = new Date(articleData.value[i].datePublished);
+// let minutes = d.getMinutes();
+// let hours = d.getHours();
+// let monthStr = month[d.getMonth()];
+// minutes = minutes > 9 ? minutes : '0' + minutes;
+// hours >= 12 ? minutes += 'PM' : minutes += 'AM'
+// hours = hours > 12 ? hours - 12 : hours;
