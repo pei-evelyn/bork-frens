ffe5eaf7320478a0d5cbfb39039bbab8cc9c0702
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-10-10 09:28:23 -0700

    Fixed date

--- client/components/other-user-bio.jsx
+++ client/components/other-user-bio.jsx
@@ -7,23 +7,23 @@ function OtherUserBio(props) {
         <form>
           <div className="form-row">
             <div className="form-group col">
-              <label>My Breed</label>
+              <label>Breed</label>
               <input className="form-control" placeholder={props.breed} disabled />
             </div>
           </div>
           <div className="form-row">
             <div className="form-group col-6">
-              <label>My Gender</label>
+              <label>Gender</label>
               <input className="form-control" placeholder={props.gender} disabled/>
             </div>
             <div className="form-group col-6">
-              <label>My Age</label>
+              <label>DOB</label>
               <input className="form-control" placeholder={props.age} disabled/>
             </div>
           </div>
           <div className="form-row">
             <div className="form-group col">
-              <label>My Frenliness Level</label>
+              <label>Frenliness Level</label>
               <input className="form-control" placeholder={props.level} disabled />
             </div>
           </div>
